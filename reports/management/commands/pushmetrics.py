7f1de0bce39d3e1a20f87dcc66913dc3bdc7514a
Alex Morega <alex@grep.ro>
2016-06-19 19:24:18 +0300

    insert only the latest data

--- reports/management/commands/pushmetrics.py
+++ reports/management/commands/pushmetrics.py
@@ -11,11 +11,24 @@ class Command(BaseCommand):
 
     help = "Push metrics data to elasticsearch"
 
+    def get_latest_doc(self):
+        res = es.search(
+            index=settings.ELASTICSEARCH_INDEX,
+            body={'sort': [{'time': 'desc'}], 'size': 1},
+        )
+        for hit in res['hits']['hits']:
+            return hit['_id']
+        return ''
+
     def handle(self, **options):
+        latest_doc = self.get_latest_doc()
+        count = 0
         for file in (metrics / 'users').iterdir():
             with file.open() as lines:
                 for n, line in enumerate(lines, 1):
                     doc_id = 'users.{}.{:06d}'.format(file.stem, n)
+                    if doc_id <= latest_doc:
+                        continue
                     data = json.loads(line)
                     data['time'] = int(data['time'] * 1000)
                     es.index(
@@ -24,4 +37,6 @@ class Command(BaseCommand):
                         id=doc_id,
                         body=data,
                     )
+                    count += 1
                 print(file.stem, n)
+        print(count)
