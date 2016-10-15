cfb40dadc973791c3494519d0c1396f6e0bbb426
Alex Morega <alex@grep.ro>
2016-10-15 16:11:18 +0300

    rename positional argument

--- reports/management/commands/pushmetrics.py
+++ reports/management/commands/pushmetrics.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-a', action='store_true', dest='all')
-        parser.add_argument('source_list', nargs='*')
+        parser.add_argument('source', nargs='*')
 
-    def handle(self, all, source_list, **options):
-        indexing.push_metrics(all, source_list)
+    def handle(self, all, source, **options):
+        indexing.push_metrics(all, source_list=source)
